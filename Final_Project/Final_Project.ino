dcc3ec486678aa12baffc9909754513cd4cc699f
perezherrera-alexander <aperezherrera@unr.nevada.edu>
2022-12-04 17:58:10 -0800

    Removed Test File and Added Main Code File

---
+++ Final_Project/Final_Project.ino
@@ -0,0 +1,103 @@
+#define RDA 0x80
+#define TBE 0x20
+// Timer Pointers
+volatile unsigned char *myTCCR1A = (unsigned char *) 0x80;
+volatile unsigned char *myTCCR1B = (unsigned char *) 0x81;
+volatile unsigned char *myTCCR1C = (unsigned char *) 0x82;
+volatile unsigned char *myTIMSK1 = (unsigned char *) 0x6F;
+volatile unsigned int  *myTCNT1  = (unsigned  int *) 0x84;
+volatile unsigned char *myTIFR1 =  (unsigned char *) 0x36;
+// GPIO Pointers
+volatile unsigned char *portDDRB = (unsigned char *) 0x24;
+volatile unsigned char *portB =    (unsigned char *) 0x25;
+// UART Pointers
+volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
+volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1; //
+volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
+volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4; // Baud Rate Registers
+volatile unsigned char *myUDR0   = (unsigned char *)0x00C6; // Data Register
+// Define Port B Register Pointers
+volatile unsigned char* port_b = (unsigned char*) 0x25; 
+volatile unsigned char* ddr_b  = (unsigned char*) 0x24; 
+volatile unsigned char* pin_b  = (unsigned char*) 0x23;
+
+unsigned char input_char;
+int frequency = 0;
+unsigned char input[8] = {'A','B','C','D','E','F','G','Q'};
+unsigned int ticks[8] = {18182,16194,15296,13629,12140,11461,10204,0};
+
+unsigned int tickCount = 0;
+bool timerIsRunning = false;
+
+void setup() 
+{
+  U0init(9600); // initialize the serial port on USART0:
+}
+void loop() 
+{
+
+}
+
+void U0init(unsigned long U0baud)
+{
+//  Students are responsible for understanding
+//  this initialization code for the ATmega2560 USART0
+//  and will be expected to be able to intialize
+//  the USART in differrent modes.
+//
+ unsigned long FCPU = 16000000;
+ unsigned int tbaud;
+ tbaud = (FCPU / 16 / U0baud - 1);
+ // Same as (FCPU / (16 * U0baud)) - 1;
+ *myUCSR0A = 0x20;
+ *myUCSR0B = 0x18;
+ *myUCSR0C = 0x06;
+ *myUBRR0  = tbaud;
+}
+//
+// Read USART0 RDA status bit and return non-zero true if set
+//
+// This function was brought over from an older lab but then was reworked into kbhit(), I refuse to remove anything in case it breaks so here it stays.
+unsigned char U0kbhit()
+{
+  return (*myUCSR0A & RDA) ? true : false;
+}
+unsigned char kbhit()
+{
+  return *myUCSR0A & RDA;
+}
+//
+// Read input character from USART0 input buffer
+//
+unsigned char U0getchar()
+{
+  //unsigned char chara = new char();
+  //chara = *myUDR0;
+  //Serial.println(*myUDR0);
+  return *myUDR0;
+}
+//
+// Wait for USART0 TBE to be set then write character to
+// transmit buffer
+//
+void U0putchar(unsigned char U0pdata)
+{
+  while((*myUCSR0A & TBE) == 0);
+    *myUDR0 = U0pdata;
+}
+
+void set_PB_as_output(unsigned char pin_num)
+{
+    *ddr_b |= 0x01 << pin_num;
+}
+void write_pb(unsigned char pin_num, unsigned char state)
+{
+  if(state == 0)
+  {
+    *port_b &= ~(0x01 << pin_num);
+  }
+  else
+  {
+    *port_b |= 0x01 << pin_num;
+  }
+}
