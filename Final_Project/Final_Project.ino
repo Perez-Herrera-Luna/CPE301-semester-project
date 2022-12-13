5e61fbed0fa3ba7c1898552565e4c96c901dfa11
perezherrera-alexander <aperezherrera@unr.nevada.edu>
2022-12-12 19:10:26 -0800

    Cleaned up code to prepare for merging

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -1,5 +1,8 @@
 #define RDA 0x80
 #define TBE 0x20
+// Sensor pins
+#define waterSensorPower 4
+#define waterSensorPin 3
 // Timer Pointers
 volatile unsigned char *myTCCR1A = (unsigned char *) 0x80;
 volatile unsigned char *myTCCR1B = (unsigned char *) 0x81;
@@ -20,14 +23,15 @@ volatile unsigned char *myUDR0   = (unsigned char *)0x00C6; // Data Register
 volatile unsigned char* port_b = (unsigned char*) 0x25; 
 volatile unsigned char* ddr_b  = (unsigned char*) 0x24; 
 volatile unsigned char* pin_b  = (unsigned char*) 0x23;
-
-unsigned char input_char;
-int frequency = 0;
-unsigned char input[8] = {'A','B','C','D','E','F','G','Q'};
-unsigned int ticks[8] = {18182,16194,15296,13629,12140,11461,10204,0};
-
-unsigned int tickCount = 0;
-bool timerIsRunning = false;
+// ADC Registers
+volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
+volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
+volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
+volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
+// Port H Register Pointers
+unsigned char* port_h = (unsigned char*) 0x102; // Port H Data Register
+unsigned char* ddr_h  = (unsigned char*) 0x101; // Port H Data Direction Register
+unsigned char* pin_h  = (unsigned char*) 0x100; // Port H Input Pins Address
 
 unsigned int systemState = 0;
 // 0 = off
@@ -35,6 +39,8 @@ unsigned int systemState = 0;
 // 2 = Idle
 // 3 = Disabled
 // 4 = Error
+
+unsigned int waterValue = 0; // Value for storing water level
 
 void setup() 
 {
@@ -56,38 +62,24 @@ void U0init(unsigned long U0baud)
  *myUCSR0C = 0x06;
  *myUBRR0  = tbaud;
 }
-//
+
 // Read USART0 RDA status bit and return non-zero true if set
-//
-// This function was brought over from an older lab but then was reworked into kbhit(), I refuse to remove anything in case it breaks so here it stays.
-unsigned char U0kbhit()
-{
-  return (*myUCSR0A & RDA) ? true : false;
-}
 unsigned char kbhit()
 {
   return *myUCSR0A & RDA;
 }
-//
 // Read input character from USART0 input buffer
-//
 unsigned char U0getchar()
 {
-  //unsigned char chara = new char();
-  //chara = *myUDR0;
-  //Serial.println(*myUDR0);
   return *myUDR0;
 }
-//
-// Wait for USART0 TBE to be set then write character to
-// transmit buffer
-//
+// Wait for USART0 TBE to be set then write character to transmit buffer
 void U0putchar(unsigned char U0pdata)
 {
   while((*myUCSR0A & TBE) == 0);
     *myUDR0 = U0pdata;
 }
-
+// These two functions probably aren't needed for anything we're doing here but better safe than sorry.
 void set_PB_as_output(unsigned char pin_num)
 {
     *ddr_b |= 0x01 << pin_num;
@@ -115,7 +107,6 @@ void set_pin_direction(unsigned char* data_direction_register, unsigned char pin
   if(mode == OUTPUT) *data_direction_register |= 0x01 << pin_num;
   else if(mode == INPUT) *data_direction_register &= ~(0x01 << pin_num);
 }
-
 /*
 * Write a HIGH or LOW value to a digital pin
 * @param data_register - The Data Register of the desired port
