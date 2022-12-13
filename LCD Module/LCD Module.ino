5302fc724ec846caa08d66092745727a0d1ff381
perezherrera-alexander <aperezherrera@unr.nevada.edu>
2022-12-12 18:51:44 -0800

    Updated port numbers being used

--- LCD Module/LCD Module.ino
+++ LCD Module/LCD Module.ino
@@ -6,7 +6,7 @@
 
 // with the arduino pin number it is connected to
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int rs = 48, en = 49, d4 = 50, d5 = 51, d6 = 52, d7 = 53;
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
@@ -14,12 +14,11 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 void setup() {
 
   // set up the LCD's number of columns and rows:
-
-  lcd.begin(16, 2);
+  lcd.begin(16, 2); // This line is neccessary, do not change
 
   // Print a message to the LCD.
 
-  lcd.print("hello, world!");
+  lcd.print("testing");
 
 }
 
@@ -27,13 +26,14 @@ void setup() {
 void loop() {
 
   // set the cursor to column 0, line 1
-
   // (note: line 1 is the second row, since counting begins with 0):
-
   lcd.setCursor(0, 1);
 
-  // print the number of seconds since reset:
+  lcd.print("Test: ");
 
+  //lcd.autoscroll();
+
+  // print the number of seconds since reset:
   lcd.print(millis() / 1000);
 
 }
