4b03e076f1445ad01a8ab1bbbbaf785492ad8766
Edward Perez-Herrera <eperezherrera@nevada.unr.edu>
2022-12-13 22:07:18 -0800

    Serial Output implementation 2nd pass

--- Serial Output/Serial Output.ino
+++ Serial Output/Serial Output.ino
@@ -12,13 +12,10 @@ void setup()
 }
 void loop()
 {
+  /*
   unsigned char cs1; // Read current char in buffer
   unsigned char MSB; //
   unsigned char LSB;
-  char test[11] = "Test Output";
-  U0putcharArray(test, 11);
-  U0putchar('a');
-  /*
   while (U0kbhit()==0){}; // wait for RDA = true
   cs1 = U0getchar();    // read character
   MSB = hexMSB(cs1);
@@ -38,68 +35,34 @@ void loop()
 
   *myUCSR0B |= 0x10; // 0b 0001 0000 enalbes USART reciever
   */
-  delay(2000);
 }
-//
-// function to initialize USART0 to "int" Baud, 8 data bits,
-// no parity, and one stop bit. Assume FCPU = 16MHz.
-//
+
+// Initalizes USART0 to "int" Baud, 8 data bits, no parity, and one stop bit. Assumes FCPU = 16MHz
 void U0init(unsigned long U0baud)
 {
-//  Students are responsible for understanding
-//  this initialization code for the ATmega2560 USART0
-//  and will be expected to be able to intialize
-//  the USART in differrent modes.
-//
- unsigned long FCPU = 16000000;
- unsigned int tbaud;
- tbaud = (FCPU / 16 / U0baud - 1);
- // Same as (FCPU / (16 * U0baud)) - 1;
- *myUCSR0A = 0x20;
- *myUCSR0B = 0x18;
- *myUCSR0C = 0x06;
- *myUBRR0  = tbaud;
+  unsigned long FCPU = 16000000;
+  unsigned int tbaud;
+  tbaud = (FCPU / 16 / U0baud - 1); // Same as (FCPU / (16 * U0baud)) - 1;
+  *myUCSR0A = 0x20;
+  *myUCSR0B = 0x18;
+  *myUCSR0C = 0x06;
+  *myUBRR0  = tbaud;
 }
-//
+
 // Read USART0 RDA status bit and return non-zero true if set
-//
 unsigned char U0kbhit()
 {
   return (*myUCSR0A & RDA) ? true : false;
 }
-//
+
 // Read input character from USART0 input buffer
-//
 unsigned char U0getchar()
 {
   return *myUDR0;
 }
-//
-// Wait for USART0 TBE to be set then write character to
-// transmit buffer
-//
+
 void U0putchar(unsigned char U0pdata)
 {
-  while((*myUCSR0A & TBE) == 0);
+  while((*myUCSR0A & TBE) == 0); // Wait for USART0 TBE to be set then write character to transmit buffer
     *myUDR0 = U0pdata;
-}
-
-void U0putcharArray(char* charArray, int length)
-{
-  for(int i = 0; i < length; i++)
-  {
-    U0putchar(charArray);
-  }
-}
-
-unsigned char hexMSB(unsigned char chara)
-{
-  unsigned char MSB = (chara / 16) + '0';
-  return MSB;
-}
-
-unsigned char hexLSB(unsigned char chara)
-{
-  unsigned char LSB = chara % 16 + '0';
-  return LSB;
 }
